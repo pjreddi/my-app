f8c600b1fbe397170f93306d0ca3d9ac6dfc43df
pjreddi <pjreddy.cicd@gmail.com>
2024-03-25 17:59:21 +0530

    initializing flask project

---
+++ start.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Welcome to my site</h1?"
+
+if __name__=='__main__':
+    app.run(debug=True,host='0.0.0.0')
+
